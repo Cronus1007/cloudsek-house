750a2f29987a291e5fb9389ddf1e353cc101bf49
Cronus <56436023+Cronus1007@users.noreply.github.com>
2023-07-31 00:31:35 +0530

    Add files via upload

---
+++ components/gridcell.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './layout.css';
+import DropdownButton from './DropdownButton';
+
+const GridCell = (props) => {
+  return (
+    <div className="grid-cell" name={props.i} value={props.j}>
+      <DropdownButton menuItems={props.menuItems} handleUpdateMenuItems={props.handleUpdateMenuItems} i={props.i} j={props.j} />
+    </div>
+  );
+};
+
+export default GridCell;
